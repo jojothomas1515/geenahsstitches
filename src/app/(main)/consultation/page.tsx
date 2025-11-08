5c32065073c279560ba2f115ff3efd5f906b6a11
Flash Sam <flashsam8@gmail.com>
2025-11-08 09:16:55 +0100

    consultation page

---
+++ src/app/(main)/consultation/page.tsx
@@ -0,0 +1,35 @@
+const Consultation = async () => {
+  return (
+    <main>
+      <section className="header bg-amber-700 p-4">
+        <h1 className="md:font-bold md:text-2xl ">Book an appointment</h1>
+      </section>
+      <section>
+        <div className="container m-auto p-4">
+          <h4>Thank You for Choosing Geenah’s Stitches</h4>
+          <p>
+            We’re thrilled that you’re interested in creating magic with us! To
+            ensure we bring your vision to life seamlessly, a consultation is
+            required before placing an order. This allows us to discuss your
+            designs, fabric choices, and measurements in detail.
+          </p>
+          <p>
+            If you’re unsure of the design you want, we recommend booking an
+            appointment to explore ideas and create something unique together.
+            However, if you already have a design in mind, simply contact us and
+            provide the details so we can bring your vision to reality.
+          </p>
+
+          <h4>Choose Your Consultation Type</h4>
+          <p>
+            Our consultation plans are tailored to suit your specific needs,
+            event type, and the urgency of your booking. Select the plan that
+            best aligns with your vision:
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Consultation;
