14c6251865fa84925ccc7449c27027e153468817
Flash Sam <flashsam8@gmail.com>
2026-03-20 09:32:37 +0100

    feat: Introduce global styling with theme variables and add a reusable StatCard component for displaying statistics.

--- src/components/dashboard/admin/StatCard.tsx
+++ src/components/dashboard/admin/StatCard.tsx
@@ -36,7 +36,7 @@ export default function StatCard({
             {href && (
                 <Link
                     href={href}
-                    className="text-sm font-medium bg-background-light text-basic rounded-lg px-4 py-2 text-center transition-colors duration-200 hover:bg-primary mt-auto"
+                    className="text-sm font-medium bg-background-light text-basic rounded-lg px-4 py-2 text-center transition-all duration-200 hover:bg-primary mt-auto shadow-sm hover:shadow-md hover:text-white/90 "
                 >
                     {linkLabel ?? "View All"}
                 </Link>
