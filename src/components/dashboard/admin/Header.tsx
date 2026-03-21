2a9c1e15b92ad22d24ecebf295bc8ce0c2566aaf
Flash Sam <flashsam8@gmail.com>
2026-03-21 22:15:39 +0100

    feat: Add role-specific dashboard header components for staff and admin users.

--- src/components/dashboard/admin/Header.tsx
+++ src/components/dashboard/admin/Header.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard, Package, ShoppingCart, Users } from "lucide-react"
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import Nav from "./Header/Nav";
+import { redirect } from "next/navigation";
 
 const navLinks = [
     { href: "/admin/dashboard", label: "Dashboard", icon: <LayoutDashboard size={20} /> },
@@ -16,17 +17,32 @@ const navLinks = [
 
 export default async function AdminHeader() {
     const session = await auth.api.getSession({ headers: await headers() });
-    console.log(session)
+    if (!session) {
+        redirect("/login")
+        return <></>
+    };
 
-    return (
-        <header className="w-full md:w-3/10  bg-background md:h-dvh h-fit p-5 " style={{ anchorName: "--header" }}>
-            <div className="w-full flex gap-10 md:p-5 md:flex-col md:justify-normal justify-between flex-row md:mt-10">
-                <div className="flex items-center gap-2">
-                    <Link href={"/"}><Image src={Logo} alt="Logo" width={1000} height={1000} className="w-60!  dark:invert object-contain" /></Link>
+    if (session.user.role === "STAFF") {
+        redirect("/staff/dashboard")
+        return <></>
+    };
+
+    if (session.user.role === "USER") {
+        redirect("/")
+        return <></>
+    };
+
+    if (session.user.role === "ADMIN") {
+        return (
+            <header className="w-full md:w-3/10  bg-background md:h-dvh h-fit p-5 " style={{ anchorName: "--header" }}>
+                <div className="w-full flex gap-10 md:p-5 md:flex-col md:justify-normal justify-between flex-row md:mt-10">
+                    <div className="flex items-center gap-2">
+                        <Link href={"/"}><Image src={Logo} alt="Logo" width={1000} height={1000} className="w-60!  dark:invert object-contain" /></Link>
+                    </div>
+
+                    <Nav navLinks={navLinks} />
                 </div>
-
-                <Nav navLinks={navLinks} />
-            </div>
-        </header>
-    )
+            </header>
+        )
+    }
 }
--- src/components/dashboard/staff/Header.tsx
+++ src/components/dashboard/staff/Header.tsx
@@ -5,6 +5,7 @@ import { LayoutDashboard, Package, ShoppingCart } from "lucide-react"
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import Nav from "./Header/Nav";
+import { redirect } from "next/navigation";
 
 const navLinks = [
     { href: "/staff/dashboard", label: "Dashboard", icon: <LayoutDashboard size={20} /> },
@@ -15,17 +16,32 @@ const navLinks = [
 
 export default async function StaffHeader() {
     const session = await auth.api.getSession({ headers: await headers() });
-    console.log(session)
+    if (!session) {
+        redirect("/login")
+        return <></>
+    };
 
-    return (
-        <header className="w-full md:w-3/10  bg-background md:h-dvh h-fit p-5 " style={{ anchorName: "--header" }}>
-            <div className="w-full flex gap-10 md:p-5 md:flex-col md:justify-normal justify-between flex-row md:mt-10">
-                <div className="flex items-center gap-2">
-                    <Link href={"/"}><Image src={Logo} alt="Logo" width={1000} height={1000} className="w-60!  dark:invert object-contain" /></Link>
+    if (session.user.role === "ADMIN") {
+        redirect("/admin/dashboard")
+        return <></>
+    };
+
+    if (session.user.role === "USER") {
+        redirect("/")
+        return <></>
+    };
+
+    if (session.user.role === "STAFF") {
+        return (
+            <header className="w-full md:w-3/10  bg-background md:h-dvh h-fit p-5 " style={{ anchorName: "--header" }}>
+                <div className="w-full flex gap-10 md:p-5 md:flex-col md:justify-normal justify-between flex-row md:mt-10">
+                    <div className="flex items-center gap-2">
+                        <Link href={"/"}><Image src={Logo} alt="Logo" width={1000} height={1000} className="w-60!  dark:invert object-contain" /></Link>
+                    </div>
+
+                    <Nav navLinks={navLinks} />
                 </div>
-
-                <Nav navLinks={navLinks} />
-            </div>
-        </header>
-    )
+            </header>
+        )
+    }
 }
